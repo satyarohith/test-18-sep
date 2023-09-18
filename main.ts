24ce899e42a3316b1865141f32fad2570d48ea6f
Satya Rohith <me@satyarohith.com>
2023-09-18 12:41:19 +0530

    update

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 function handler(_req: Request): Response {
-  return new Response("Hello, World - this is a another massive update!");
+  return new Response("Hello, World - this is just a small update!");
 }
 
 Deno.serve(handler);
